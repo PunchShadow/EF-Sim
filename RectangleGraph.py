4e92b975c23f8305d51f2d30f2725f4d7f1170b1
PunchShadow <littleuniverse24@gmail.com>
2019-06-09 19:53:53 +0800

    Add configparser to RectangleGraph.py and modify hardware description

--- RectangleGraph.py
+++ RectangleGraph.py
@@ -1,6 +1,27 @@
 import networkx as nx
+import configparser as config
 import math
 import sys
+
+config = config.ConfigParser()
+config.read('config/network.cfg')
+
+PE = 'PENode'
+Mem = 'MemoryNode'
+Bus = 'Bus'
+Global = 'DRAM'
+
+PE_sram = config.get(PE, 'SRAMsize')
+PE_energy = config.get(PE, 'EnergyCost')
+PE_cycle = config.get(PE, 'ComputTime')
+Mem_dram = config.get(Mem, 'DRAMsize')
+Mem_energy = config.get(Mem, 'EnergyCost')
+Mem_cycle = config.get(Mem, 'TransferTime')
+Bus_cycle = config.get(Bus, 'Latency')
+Bus_energy = config.get(Bus, 'EnergyCost')
+Global_dram = config.get(Global, 'DRAMsize')
+Global_energy = config.get(Global, 'EnergyCost')
+Global_cycle = config.get(Global, 'TransferTime')
 
 # Initialize the graph
 # arg[0] = dimension (determine how many arguments are there)
@@ -28,28 +49,28 @@ def createRecGraph(*arg):
     # 2D matrix
     if(arg[0] == 2):
         for i in range(node_num):
-            G.add_edge('G',i, gid=0, energy=2.0, time=1.0)
+            G.add_edge('G',i, gid=0, energy=Global_energy, time=Global_cycle)
             row = i//arg[2]
             col = i%arg[2]
             if(col+1 < arg[2]):
-                G.add_edge(i, i+1, gid=1, energy=0.5, time=0.1)
+                G.add_edge(i, i+1, gid=1, energy=Bus_energy, time=Bus_cycle)
             if(row+1 < arg[1]):
-                G.add_edge(i, i+arg[2], gid=2, energy=0.5, time=0.1)
+                G.add_edge(i, i+arg[2], gid=2, energy=Bus_energy, time=Bus_cycle)
 
     # 3D matrix
     elif(arg[0] == 3):
         plane_num = arg[1]*arg[2] #One plane's node number.
         for i in range(node_num):
-            G.add_edge('G', i, gid=0, energy=2.0, time=1.0)
+            G.add_edge('G', i, gid=0, energy=Global_energy, time=Global_cycle)
             row =(i%plane_num)//arg[2]
             col = (i%plane_num)%arg[2]
             height = i//plane_num
             if(col+1 < arg[2]):
-                G.add_edge(i, i+1, gid=1, energy=0.5, time=0.1)
+                G.add_edge(i, i+1, gid=1, energy=Bus_energy, time=Bus_cycle)
             if(row+1 < arg[1]):
-                G.add_edge(i, i+arg[2], gid=1, energy=0.5, time=0.2)
+                G.add_edge(i, i+arg[2], gid=1, energy=Bus_energy, time=Bus_cycle)
             if(height+1 < arg[3]):
-                G.add_edge(i, i+plane_num, gid=1, energy=0.5, time=0.3)
+                G.add_edge(i, i+plane_num, gid=1, energy=Bus_energy, time=Bus_cycle)
 
     else:
         print("Dimension Error!!!")
@@ -65,7 +86,7 @@ def eyeriss(config): #12*14
     for j in range(14):
         for i in range(12):
             v=i*14+j
-            G.add_edge('G', v, gid=i, energy=0.5, time=0.1) # filter edge
+            G.add_edge('G', v, gid=i, energy=Bus_energy, time=Bus_cycle) # filter edge
             G.add_edge('G', v, gid=12+config[i][j], energy=1.0, time=0.4) # imap edge
             if(i!=0):
                 G.add_edge(v, v-14, gid=50, energy=1.0, time=0.3) # omap edge
@@ -75,5 +96,6 @@ def eyeriss(config): #12*14
 # edgelist example: [(2,3), (1,4), (3,9), ....]
 def addGroup(graph, group, *edgelist):
     graph.add_edges_from(edgelist, group = group)
+
 
 G = createRecGraph(2,5,5)
