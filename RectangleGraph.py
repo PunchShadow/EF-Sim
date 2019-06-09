f95762c2bdf6d21cede005288f716040d6753ca8
qazwsxedcrfvtg14 <qazwsxedcrfvtg14@gmail.com>
2019-06-09 17:46:45 +0800

    add eyeriss

--- RectangleGraph.py
+++ RectangleGraph.py
@@ -22,48 +22,57 @@ def createRecGraph(*arg):
     for i in range(arg[0]):
         node_num *= arg[i+1]
     for i in range(node_num):
-        G.add_node(i);
+        G.add_node(i)
     
     # Initialize the edges of all nodes.
     # 2D matrix
     if(arg[0] == 2):
-        for i in range(node_num-1):
-            G.add_edge(i, 'G')
-            row = math.floor(i/arg[2])
+        for i in range(node_num):
+            G.add_edge('G',i, gid=0, energy=2.0)
+            row = i//arg[2]
             col = i%arg[2]
-            if(col+1 <= arg[2]-1):
-                G.add_edge(i, i+1)
-            if(row+1 <= arg[1]-1):
-                G.add_edge(i, i+arg[2])
+            if(col+1 < arg[2]):
+                G.add_edge(i, i+1, gid=1, energy=0.5)
+            if(row+1 < arg[1]):
+                G.add_edge(i, i+arg[2], gid=2, energy=0.5)
 
-        G.add_edge(node_num-1, 'G')
     # 3D matrix
     elif(arg[0] == 3):
         plane_num = arg[1]*arg[2] #One plane's node number.
-        for i in range(node_num-1):
-            G.add_edge(i, 'G')
-            row = math.floor((i%plane_num)/arg[2])
+        for i in range(node_num):
+            G.add_edge('G', i, gid=0, energy=2.0)
+            row =(i%plane_num)//arg[2]
             col = (i%plane_num)%arg[2]
-            height = math.floor(i/plane_num)
-           
-            if(col+1 <= arg[2]-1):
-                G.add_edge(i, i+1)
-            if(row+1 <= arg[1]-1):
-                G.add_edge(i, i+arg[2])
-            if(height+1 <= arg[3]-1):
-                G.add_edge(i, i+plane_num)
-        G.add_edge(node_num-1, 'G')
+            height = i//plane_num
+            if(col+1 < arg[2]):
+                G.add_edge(i, i+1, gid=1, energy=0.5)
+            if(row+1 < arg[1]):
+                G.add_edge(i, i+arg[2], gid=1, energy=0.5)
+            if(height+1 < arg[3]):
+                G.add_edge(i, i+plane_num, gid=1, energy=0.5)
+
     else:
         print("Dimension Error!!!")
         sys.exit(2)
-    
-    
+
     return G
+
+def eyeriss(config): #12*14
+    G = nx.Graph()
+    G.add_node('G') # Global buffer's node
+    for i in range(12*14):
+        G.add_node(i)
+    for j in range(14):
+        for i in range(12):
+            v=i*14+j
+            G.add_edge('G', v, gid=i, energy=0.5) # filter edge
+            G.add_edge('G', v, gid=12+config[i][j], energy=1.0) # imap edge
+            if(i!=0):
+                G.add_edge(v, v-14, gid=50, energy=1.0) # omap edge
 
 
 # edgelist example: [(2,3), (1,4), (3,9), ....]
 def addGroup(graph, group, *edgelist):
     graph.add_edges_from(edgelist, group = group)
 
-
-
+G = createRecGraph(2,5,5)
