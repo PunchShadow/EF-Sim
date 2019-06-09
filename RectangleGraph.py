727e1924b12390ea383c4f0fb3c94ad2964115cd
qazwsxedcrfvtg14 <qazwsxedcrfvtg14@gmail.com>
2019-06-09 18:25:08 +0800

    add FlowControl

--- RectangleGraph.py
+++ RectangleGraph.py
@@ -15,7 +15,7 @@ def createRecGraph(*arg):
     if((len(arg) != (arg[0]+1)) or (arg[0] == 1)):
         print("Initialization fault !! Please check your argument numbers!")
         sys.exit(1)
-    G = nx.Graph()
+    G = nx.MultiDiGraph()
     G.add_node('G') # Global buffer's node
     node_num = 1
     
@@ -28,28 +28,28 @@ def createRecGraph(*arg):
     # 2D matrix
     if(arg[0] == 2):
         for i in range(node_num):
-            G.add_edge('G',i, gid=0, energy=2.0)
+            G.add_edge('G',i, gid=0, energy=2.0, time=1.0)
             row = i//arg[2]
             col = i%arg[2]
             if(col+1 < arg[2]):
-                G.add_edge(i, i+1, gid=1, energy=0.5)
+                G.add_edge(i, i+1, gid=1, energy=0.5, time=0.1)
             if(row+1 < arg[1]):
-                G.add_edge(i, i+arg[2], gid=2, energy=0.5)
+                G.add_edge(i, i+arg[2], gid=2, energy=0.5, time=0.1)
 
     # 3D matrix
     elif(arg[0] == 3):
         plane_num = arg[1]*arg[2] #One plane's node number.
         for i in range(node_num):
-            G.add_edge('G', i, gid=0, energy=2.0)
+            G.add_edge('G', i, gid=0, energy=2.0, time=1.0)
             row =(i%plane_num)//arg[2]
             col = (i%plane_num)%arg[2]
             height = i//plane_num
             if(col+1 < arg[2]):
-                G.add_edge(i, i+1, gid=1, energy=0.5)
+                G.add_edge(i, i+1, gid=1, energy=0.5, time=0.1)
             if(row+1 < arg[1]):
-                G.add_edge(i, i+arg[2], gid=1, energy=0.5)
+                G.add_edge(i, i+arg[2], gid=1, energy=0.5, time=0.2)
             if(height+1 < arg[3]):
-                G.add_edge(i, i+plane_num, gid=1, energy=0.5)
+                G.add_edge(i, i+plane_num, gid=1, energy=0.5, time=0.3)
 
     else:
         print("Dimension Error!!!")
@@ -58,17 +58,18 @@ def createRecGraph(*arg):
     return G
 
 def eyeriss(config): #12*14
-    G = nx.Graph()
+    G = nx.MultiDiGraph()
     G.add_node('G') # Global buffer's node
     for i in range(12*14):
         G.add_node(i)
     for j in range(14):
         for i in range(12):
             v=i*14+j
-            G.add_edge('G', v, gid=i, energy=0.5) # filter edge
-            G.add_edge('G', v, gid=12+config[i][j], energy=1.0) # imap edge
+            G.add_edge('G', v, gid=i, energy=0.5, time=0.1) # filter edge
+            G.add_edge('G', v, gid=12+config[i][j], energy=1.0, time=0.4) # imap edge
             if(i!=0):
-                G.add_edge(v, v-14, gid=50, energy=1.0) # omap edge
+                G.add_edge(v, v-14, gid=50, energy=1.0, time=0.3) # omap edge
+    return G
 
 
 # edgelist example: [(2,3), (1,4), (3,9), ....]
